e054d94be854ff811a5679d77782d28e3b5f922f
Iulianna_Stepanova <Iulianna_Stepanova@epam.com>
2019-08-02 18:01:32 +0300

    constants added file

---
+++ src/constants/FormConstants.ts
@@ -0,0 +1,16 @@
+export const USER_NAME_LABEL = "Your name";
+export const USER_NAME_PROPERTY = "username";
+export const USER_NAME_PLACEHOLDER = "Username";
+export const USER_NAME_VALIDATION_TEXT = "Please input your username!";
+export const EMAIL_LABEL = "Your e-mail";
+export const EMAIL_PROPERTY = "email";
+export const EMAIL_PLACEHOLDER = "E-mail";
+export const EMAIL_VALIDATION_TEXT = "Please input your E-mail!";
+export const ARTIST_NAME_LABEL = "Artist name";
+export const ARTIST_NAME_PROPERTY = "artist";
+export const ARTIST_NAME_PLACEHOLDER = "Artist name";
+export const ARTIST_NAME_VALIDATION_TEXT = "Please select artist name!";
+export const MESSAGE_LABEL = "Your message";
+export const MESSAGE_PROPERTY = "message";
+export const MESSAGE_VALIDATION_TEXT = "Please, inter your feedback!";
+
