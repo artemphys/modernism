d81ba2fe1131d2eee92eef77af9ca0dd8cc10b19
River2015 <stjuvi@mail.ru>
2019-08-15 16:46:36 +0300

    form replaced to modal, const feedbackList renamed to data (#34)
    * form replaced to modal, const feedbackList renamed to data
    
    * bugs fix
    
    * toggleModal added
    
    * code refactoring
    
    * code refactoring

--- src/modules/FeedbackPage/index.tsx
+++ src/modules/FeedbackPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Select, Input, Button, Typography } from "antd";
+import { Form, Select, Input, Button, Typography, Modal } from "antd";
 import { FEEDBACK_ARTIST_DICTIONARY } from "../../mock";
 import {
   ARTIST_NAME_LABEL,
@@ -24,18 +24,18 @@ import { FeedbackItem } from "./FeedbackItem";
 
 interface Props {
   form: any;
-  data: any;
 }
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
 const { Option } = Select;
 const { Title } = Typography;
-const data = FEEDBACK_ARTIST_DICTIONARY;
+const dictionary = FEEDBACK_ARTIST_DICTIONARY;
 
 class FeedbackForm extends Component<Props> {
   state = {
-    feedbackList: []
+    data: [],
+    visible: false
   };
 
   handleSubmit = (e: any) => {
@@ -44,11 +44,9 @@ class FeedbackForm extends Component<Props> {
 
     validateFields((err: any, values: any) => {
       if (!err) {
+        const color = this.getBackground();
         this.setState({
-          feedbackList: [
-            ...this.state.feedbackList,
-            { ...values, color: this.getBackground() }
-          ]
+          data: [...this.state.data, { ...values, color }]
         });
 
         resetFields();
@@ -65,13 +63,16 @@ class FeedbackForm extends Component<Props> {
     return color.join("");
   };
 
-  renderFeedbackItem = (feedbackList: any) => (
-    <FeedbackItem feedbackList={feedbackList} />
-  );
+  renderFeedbackItem = (data: any) => <FeedbackItem data={data} />;
+
+  toggleModal = () =>
+    this.setState({
+      visible: !this.state.visible
+    });
 
   render() {
     const { getFieldDecorator } = this.props.form;
-    const { feedbackList } = this.state;
+    const { data } = this.state;
 
     return (
       <div className="feedback-page">
@@ -81,56 +82,66 @@ class FeedbackForm extends Component<Props> {
             you have a little time, please leave a review on museums or art
             galleries, to help other people in decision.
           </Title>
-          <Form onSubmit={this.handleSubmit}>
-            <FormItem label={USER_NAME_LABEL}>
-              {getFieldDecorator(USER_NAME_PROPERTY, {
-                rules: [{ required: true, message: USER_NAME_VALIDATION_TEXT }]
-              })(<Input placeholder={USER_NAME_PLACEHOLDER} />)}
-            </FormItem>
-            <FormItem label={EMAIL_LABEL}>
-              {getFieldDecorator(EMAIL_PROPERTY, {
-                rules: [{ required: true, message: EMAIL_VALIDATION_TEXT }]
-              })(<Input placeholder={EMAIL_PLACEHOLDER} />)}
-            </FormItem>
-            <FormItem label={ARTIST_NAME_LABEL}>
-              {getFieldDecorator(ARTIST_NAME_PROPERTY, {
-                rules: [
-                  { required: true, message: ARTIST_NAME_VALIDATION_TEXT }
-                ]
-              })(
-                <Select
-                  defaultValue=".com"
-                  placeholder={ARTIST_NAME_PLACEHOLDER}
-                >
-                  {data.map((item, i) => {
-                    return (
+          <Button type="primary" onClick={this.toggleModal}>
+            Add feedback
+          </Button>
+          <Modal
+            title="Basic Modal"
+            visible={this.state.visible}
+            footer={null}
+            onCancel={this.toggleModal}
+          >
+            <Form onSubmit={this.handleSubmit}>
+              <FormItem label={USER_NAME_LABEL}>
+                {getFieldDecorator(USER_NAME_PROPERTY, {
+                  rules: [
+                    { required: true, message: USER_NAME_VALIDATION_TEXT }
+                  ]
+                })(<Input placeholder={USER_NAME_PLACEHOLDER} />)}
+              </FormItem>
+              <FormItem label={EMAIL_LABEL}>
+                {getFieldDecorator(EMAIL_PROPERTY, {
+                  rules: [{ required: true, message: EMAIL_VALIDATION_TEXT }]
+                })(<Input placeholder={EMAIL_PLACEHOLDER} />)}
+              </FormItem>
+              <FormItem label={ARTIST_NAME_LABEL}>
+                {getFieldDecorator(ARTIST_NAME_PROPERTY, {
+                  rules: [
+                    { required: true, message: ARTIST_NAME_VALIDATION_TEXT }
+                  ]
+                })(
+                  <Select
+                    defaultValue=".com"
+                    placeholder={ARTIST_NAME_PLACEHOLDER}
+                  >
+                    {dictionary.map((item, i) =>
                       <Option value={item} key={`${i}_${item}`}>
                         {item}
                       </Option>
-                    );
-                  })}
-                </Select>
-              )}
-            </FormItem>
-            <FormItem label={MESSAGE_LABEL}>
-              {getFieldDecorator(MESSAGE_PROPERTY, {
-                rules: [{ required: true, message: MESSAGE_VALIDATION_TEXT }]
-              })(<TextArea rows={4} />)}
-            </FormItem>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              Send feedback
-            </Button>
-          </Form>
+                    )}
+                  </Select>
+                )}
+              </FormItem>
+              <FormItem label={MESSAGE_LABEL}>
+                {getFieldDecorator(MESSAGE_PROPERTY, {
+                  rules: [{ required: true, message: MESSAGE_VALIDATION_TEXT }]
+                })(<TextArea rows={4} />)}
+              </FormItem>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Send feedback
+              </Button>
+            </Form>
+          </Modal>
         </div>
         <div className="feedback-list-block">
           <Title level={3}>{`The most actual feedbacks ${
-            feedbackList.length
+            data.length
           } replies left`}</Title>
-          {feedbackList.map(this.renderFeedbackItem)}
+          {data.map(this.renderFeedbackItem)}
         </div>
       </div>
     );
