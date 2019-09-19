eae8cdc16bf80e7b98f693a4ddb1616e3764c173
River2015 <stjuvi@mail.ru>
2019-09-19 17:54:54 +0300

    Master 37 add event (#38)
    * notification added, condition for hiding text with length more then 160px added
    
    * format
    
    * component tabs changed (new item-tab added). Component Event added.

--- src/modules/MainTabs/index.tsx
+++ src/modules/MainTabs/index.tsx
@@ -2,27 +2,27 @@ import React, { Component } from "react";
 import { Tabs } from "antd";
 const TabPane = Tabs.TabPane;
 
+const TABS = ["/artists", "/genres", "/events"];
+
 interface Props {
   history: any;
 }
 
 export class MainTabs extends Component<Props> {
-  onChange = () => {
-    let { history } = this.props;
-    let url = history.location.pathname.includes("artists")
-      ? "/genres"
-      : "/artists";
-    history.push(url);
-  };
+  onChange = (activeKey: any) => this.props.history.push(TABS[activeKey - 1]);
 
-  getActiveTab = () =>
-    this.props.history.location.pathname.includes("artists") ? "1" : "2";
+  getActiveTab = () => {
+    let { pathname } = this.props.history.location;
+    let activeTab = TABS.indexOf(pathname);
+    return (activeTab + 1).toString();
+  };
 
   render() {
     return (
       <Tabs onChange={this.onChange} activeKey={this.getActiveTab()}>
         <TabPane tab="Artists" key="1" />
         <TabPane tab="Genres" key="2" />
+        <TabPane tab="Events" key="3" />
       </Tabs>
     );
   }
